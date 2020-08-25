3a1c21c0db355b12ef5ce2a8f7c4488750d581bc
Pooja shaji <poojashaji321@gmail.com>
2020-08-25 19:05:52 +0530

    third commit

--- app/src/main/java/com/android/gitsample/MainActivity.java
+++ app/src/main/java/com/android/gitsample/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("ggf","gdjagi");
         Log.d("djdi","wueiy i");
+        Log.d("wrue","rqey");
     }
 }
